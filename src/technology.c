153c9c3320985238494911c85a42b11fc887cea4
Chengyi Zhao <chengyi1.zhao@archermind.com>
2013-07-11 10:00:31 +0800

    Tethering: Add hidden access point support in technology
    Change-Id: I73fccf5f322ee2597f8f58d5e3d7f60ddeb0a641

--- src/technology.c
+++ src/technology.c
@@ -66,6 +66,7 @@ struct connman_technology {
 					      */
 	char *tethering_ident;
 	char *tethering_passphrase;
+	bool tethering_hidden;
 
 	bool enable_persistent; /* Save the tech state */
 
@@ -233,9 +234,11 @@ static int set_tethering(struct connman_technology *technology,
 	int err;
 	const char *ident, *passphrase, *bridge;
 	GSList *tech_drivers;
+	bool hidden;
 
 	ident = technology->tethering_ident;
 	passphrase = technology->tethering_passphrase;
+	hidden = technology->tethering_hidden;
 
 	__sync_synchronize();
 	if (!technology->enabled)
@@ -256,7 +259,7 @@ static int set_tethering(struct connman_technology *technology,
 			continue;
 
 		err = driver->set_tethering(technology, ident, passphrase,
-				bridge, enabled);
+				bridge, enabled, hidden);
 
 		if (result == -EINPROGRESS)
 			continue;
@@ -522,6 +525,11 @@ static void append_properties(DBusMessageIter *iter,
 		connman_dbus_dict_append_basic(&dict, "TetheringPassphrase",
 					DBUS_TYPE_STRING,
 					&technology->tethering_passphrase);
+
+	val = technology->tethering_hidden;
+	connman_dbus_dict_append_basic(&dict, "Hidden",
+					DBUS_TYPE_BOOLEAN,
+					&val);
 
 	connman_dbus_dict_close(iter, &dict);
 }
@@ -929,6 +937,18 @@ static DBusMessage *set_property(DBusConnection *conn,
 						&technology->tethering_passphrase);
 			}
 		}
+	} else if (g_str_equal(name, "Hidden")) {
+		bool hidden;
+
+		if (type != DBUS_TYPE_BOOLEAN)
+			return __connman_error_invalid_arguments(msg);
+
+		dbus_message_iter_get_basic(&value, &hidden);
+
+		if (technology->type != CONNMAN_SERVICE_TYPE_WIFI)
+			return __connman_error_not_supported(msg);
+
+		technology->tethering_hidden = hidden;
 	} else if (g_str_equal(name, "Powered")) {
 		dbus_bool_t enable;
 
@@ -1197,6 +1217,7 @@ static struct connman_technology *technology_get(enum connman_service_type type)
 
 	technology->refcount = 1;
 	technology->type = type;
+	technology->tethering_hidden = FALSE;
 	technology->path = g_strdup_printf("%s/technology/%s",
 							CONNMAN_PATH, str);
 	if (type == CONNMAN_SERVICE_TYPE_P2P) {
